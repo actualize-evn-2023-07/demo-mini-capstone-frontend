9ac26c84d5aa3b03172ab2fd24db15fc58f4d17c
Peter Jang <peter.x.jang@gmail.com>
2023-10-05 18:47:02 -0500

    Style products in a grid

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -3,14 +3,16 @@ export function ProductsIndex(props) {
   return (
     <div>
       <h1>All products</h1>
-      {props.products.map((product) => (
-        <div key={product.id}>
-          <img src={product.images[0].url} alt="" />
-          <h2>{product.name}</h2>
-          <p>Price: {product.price}</p>
-          <button>Show more</button>
-        </div>
-      ))}
+      <div className="cards">
+        {props.products.map((product) => (
+          <div key={product.id} className="card">
+            <img src={product.images[0].url} alt="" />
+            <h2>{product.name}</h2>
+            <p>Price: {product.price}</p>
+            <button>Show more</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
