f73aa0cb3de5c41e2b41ae377fd7c077976ace1c
Peter Jang <peter.x.jang@gmail.com>
2023-10-10 19:45:21 -0500

    Add dynamic search for products

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,17 +1,27 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 export function ProductsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div>
       <h1>All products</h1>
+      <div>
+        Search by title:{" "}
+        <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      </div>
       <div className="cards">
-        {props.products.map((product) => (
-          <div key={product.id} className="card">
-            <img src={product.images[0].url} alt="" />
-            <h2>{product.name}</h2>
-            <p>Price: {product.price}</p>
-            <a href={`/products/${product.id}`}>More info</a>
-          </div>
-        ))}
+        {props.products
+          .filter((product) => product.name.toLowerCase().includes(searchFilter.toLowerCase()))
+          .map((product) => (
+            <div key={product.id} className="card">
+              <img src={product.images[0].url} alt="" />
+              <h2>{product.name}</h2>
+              <p>Price: {product.price}</p>
+              <a href={`/products/${product.id}`}>More info</a>
+            </div>
+          ))}
       </div>
     </div>
   );
